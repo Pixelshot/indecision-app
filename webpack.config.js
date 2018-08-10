a3d699ef06a2644a965300b98131a2a8b0e09394
Hazri Jordan <hazrijordan@gmail.com>
2018-08-10 12:17:59 +0800

    Added extract-text-plugin to separte CSS files for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,59 @@
 
 // The path module provides utilities for working with file and directory paths. It can be accessed using:
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    // For more info on path, see section 6 video #50.
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        // This is a regex to check all files that ends with .js.
-        // For more info on this, see section 6 video #54.
-        exclude: /node_modules/
-        // Exclude is as it says, excludes any file we don't want to check.
-      },
-      {
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
+module.exports = env => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      // For more info on path, see section 6 video #50.
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          // This is a regex to check all files that ends with .js.
+          // For more info on this, see section 6 video #54.
+          exclude: /node_modules/
+          // Exclude is as it says, excludes any file we don't want to check.
+        },
+        {
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          }),
           // 'loader' is used when we want to load only one loader.
           // 'use' is used when we want to load multiple loaders.
-        ],
-        test: /\.s?css$/
-      }
-    ]
-  },
-  devtool: "cheap-module-eval-source-map",
-  // This is to setup webpack's equivalent of byebug. !Very important.
-  devServer: {
-    contentBase: path.join(__dirname, "public")
-  }
-  // For more info on devServer see Section 6 video #57.
+          test: /\.s?css$/
+        }
+      ]
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    // This is to setup webpack's equivalent of byebug. !Very important.
+    devServer: {
+      contentBase: path.join(__dirname, "public")
+    }
+    // For more info on devServer see Section 6 video #57.
+  };
 };
 
 // See documentation for more on webpack on www.webpack.js.org(under documentation tab)
